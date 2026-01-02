deaffec24c9be4ccbb22a38e210affac38c5a15f
amitpoitrix <amit.poitrix@gmail.com>
2026-01-02 21:10:37 +0530

    Added 02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <h1>Amit Kumar Behera</h1>
+        <p> Software Engineer </p>
+    </div>
+  )
+}
+
+export default Card
